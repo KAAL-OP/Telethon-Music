46b98f687f68bbd1503fc5b4bee607ba9e6891be
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-07-15 13:09:07 +0530

    Update start.py

--- Murat/plugins/start.py
+++ Murat/plugins/start.py
@@ -1,10 +1,10 @@
-from Zaid import Zaid
+from Murat import Murat
 from telethon import events, Button
 
-@Zaid.on(events.NewMessage(incoming=True, pattern="/start"))
+@Murat.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
-    await event.reply("Hello! Welcome To Music Bot Based On Telethon",
+    await event.reply("Hello! Welcome To Music Bot Based On Telethon,credit- Boo",
                     buttons=[
-                        [Button.url("⚙️Support", url="https://t.me/TheSupportChat")],
-                        [Button.url("🤖Repo", url="https://github.com/ITZ-ZAID/Telethon-Music")],
+                        [Button.url("⚙️Support", url="https://t.me/kaalxsupport")],
+                        [Button.url("🤖Repo", url="https://github.com/kaal0408/Telethon-Music")],
                     ])
