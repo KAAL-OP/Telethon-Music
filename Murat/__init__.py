4b595daff0b8f1e2e2387d406137ff08c8a32ce9
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-07-15 13:00:24 +0530

    Update __init__.py

--- Murat/__init__.py
+++ Murat/__init__.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 from Config import Config
 
 bot = TelegramClient('Zaid', api_id=Config.API_ID, api_hash=Config.API_HASH)
-Zaid = bot.start(bot_token=Config.BOT_TOKEN)
+Murat = bot.start(bot_token=Config.BOT_TOKEN)
 client = TelegramClient(StringSession(Config.STRING_SESSION), Config.API_ID, Config.API_HASH)
 call_py = PyTgCalls(client)
 client.start()
